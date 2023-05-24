abbe755834806e6ced8340b11e7d7723875f4cd7
yusuke420 <yusukehata6@gmail.com>
2023-05-24 10:48:58 +0900

    node_modulesのコンパイルエラーと格闘、解消

---
+++ resources/views/admin/auth/edit.blade.php
@@ -0,0 +1,120 @@
+<x-admin-layout>
+    <div class="bg-white py-6 sm:py-8 lg:py-12">
+        <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+            <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">周知内容編集投稿</h2>
+            <form method="POST" action="{{ route('admin.post.update') }}" class="mx-auto max-w-lg rounded-lg border">
+                @csrf
+                <div class="flex flex-col gap-4 p-4 md:p-8">
+                    <!-- Title -->
+                    <div>
+                        <x-input-label for="title" :value="__('タイトル')" />
+                        @empty(old('title'))
+                            <input id="title" class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" type="text" name="title" value="{{ $post['title'] }}" required autofocus autocomplete="title" />
+                        @else
+                            <input id="title" class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" type="text" name="title" value="{{ old('title') }}" required autofocus autocomplete="title" />
+                        @endempty
+                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                    </div>
+
+                    <!-- Importance -->
+                    <div>
+                        <x-input-label for="importance" :value="__('重要度')" />
+                        @empty(old('importance'))
+                            <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white" id="importance" type="importance" name="importance" required autocomplete="importance">
+                                <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                    <div class="flex items-center pl-3">
+                                        <input id="importance-high" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" type="radio" name="importance" value="high" checked="{{ $post['importance'] == '1' ? 'checked' : '' }}" required autocomplete="importance" />
+                                        <label for="importance-high" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">高</label>
+                                    </div>
+                                </li>
+                                <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                    <div class="flex items-center pl-3">
+                                        <input id="importance-mid" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" type="radio" name="importance" value="medium" checked="{{ $post['importance'] == '2' ? 'checked' : '' }}" required autocomplete="importance" />
+                                        <label for="importance-mid" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">中</label>
+                                    </div>
+                                </li>
+                                <li class="w-full dark:border-gray-600">
+                                    <div class="flex items-center pl-3">
+                                        <input id="importance-low" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" type="radio" name="importance" value="low" checked="{{ $post['importance'] == '3' ? 'checked' : '' }}" required autocomplete="importance" />
+                                        <label for="importance-low" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">低</label>
+                                    </div>
+                                </li>
+                            </ul>
+                        @else
+                            <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white" id="importance" type="importance" name="importance" required autocomplete="importance">
+                                <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                    <div class="flex items-center pl-3">
+                                        <input id="importance-high" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" type="radio" name="importance" value="high" checked="{{ old ('importance') == '1' ? 'checked' : '' }}" required autocomplete="importance" />
+                                        <label for="importance-high" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">高</label>
+                                    </div>
+                                </li>
+                                <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                                    <div class="flex items-center pl-3">
+                                        <input id="importance-mid" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" type="radio" name="importance" value="medium" checked="{{ old ('importance') == '2' ? 'checked' : '' }}" required autocomplete="importance" />
+                                        <label for="importance-mid" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">中</label>
+                                    </div>
+                                </li>
+                                <li class="w-full dark:border-gray-600">
+                                    <div class="flex items-center pl-3">
+                                        <input id="importance-low" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" type="radio" name="importance" value="low" checked="{{ old ('importance') == '3' ? 'checked' : '' }}" required autocomplete="importance" />
+                                        <label for="importance-low" class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">低</label>
+                                    </div>
+                                </li>
+                            </ul>
+                        @endif
+                        <x-input-error :messages="$errors->get('importance')" class="mt-2" />
+                    </div>
+
+                    <!-- Body -->
+                    <div>
+                        <x-input-label for="body" :value="__('内容')" />
+                        <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                            <div class="flex items-center justify-between px-3 py-2 border-b dark:border-gray-600">
+                                <div class="flex flex-wrap items-center divide-gray-200 sm:divide-x dark:divide-gray-600">
+                                    <div class="flex items-center space-x-1 sm:pr-4">
+                                        <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd"></path></svg>
+                                            <span class="sr-only">Attach file</span>
+                                        </button>
+                                        <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path></svg>
+                                            <span class="sr-only">Embed map</span>
+                                        </button>
+                                        <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd"></path></svg>
+                                            <span class="sr-only">Upload image</span>
+                                        </button>
+                                        <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                            <span class="sr-only">Format code</span>
+                                        </button>
+                                        <button type="button" class="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z" clip-rule="evenodd"></path></svg>
+                                            <span class="sr-only">Add emoji</span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="tooltip-fullscreen" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+                                Show full screen
+                                <div class="tooltip-arrow" data-popper-arrow></div>
+                            </div>
+                            <div class="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800" required autocomplete="body">
+                                @empty(old('body'))
+                                    <textarea id="body" name="body" rows="8" class="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" required autocomplete="body">{{ $post['body'] }}</textarea>
+                                @else
+                                    <textarea id="body" name="body" rows="8" class="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" required autocomplete="body">{{ old('body') }}</textarea>
+                                @endempty
+                            </div>
+                        </div>
+                        <x-input-error :messages="$errors->get('body')" class="mt-2" />
+                    </div>
+
+                    <x-primary-button class="block rounded-lg bg-gray-800 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-gray-700 focus-visible:ring active:bg-gray-600 md:text-base">
+                        {{ __('編集') }}
+                    </x-primary-button>
+                </div>
+            </form>
+        </div>
+    </div>
+</x-admin-layout>
