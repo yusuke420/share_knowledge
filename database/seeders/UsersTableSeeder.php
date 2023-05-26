b49999525e4b17068bac2a47d2f00f3832e259ca
yusuke420 <yusukehata6@gmail.com>
2023-05-26 10:52:33 +0900

    ver.1

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -16,9 +16,15 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'test1',
-            'email' => 'test1@test.com',
-            'password' => bcrypt('11111111') 
+            [
+                'name' => 'test1',
+                'email' => 'test1@test.com',
+                'password' => bcrypt('11111111') 
+            ],[
+                'name' => 'test2',
+                'email' => 'test2@test.com',
+                'password' => bcrypt('22222222')
+            ]
         ]);
     }
 }
